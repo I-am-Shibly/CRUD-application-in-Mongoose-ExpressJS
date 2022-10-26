3159a79c8d28add94dcb7595e80269c63db1bc27
Samiul Islam Shibly <samiul.shibli01@gmail.com>
2022-10-26 09:50:22 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const todoHandler = require('./routeHandler/todoHandler')
+
+// express app initialization
+const app = express()
+app.use(express.json())
+
+// database connection with mongoose
+mongoose.connect('mongodb://localhost/todos')
+    .then(() => console.log('connection successful!'))
+    .catch(err => console.log(err))
+
+// application routes
+app.use('/todo', todoHandler)
+
+function errorHandler(err, req, res, next) {
+    if (res.headersSent) {
+        return next(err)
+    }
+    res.status(500).json({ error: err })
+}
+
+app.listen(3000, () => {
+    console.log('listening on 3000');
+})
